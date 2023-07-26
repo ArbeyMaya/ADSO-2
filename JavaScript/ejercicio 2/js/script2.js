523d472ba408b9f225dcfead7764d7f414cf8ffb
ArbeyMaya <vilmarbey@gmail.com>
2023-07-25 22:03:37 -0500

    se actualizo carperta

---
+++ JavaScript/ejercicio 2/js/script2.js
@@ -0,0 +1,19 @@
+/*Dado el sueldo de un empleado
+imprimir en una alerta el nuevo sueldo
+del empleado si tuvo un aumento del 10% */
+
+
+/*let sueldo = prompt("ingrese el valor del sueldo");
+let psueldo= 10/100;
+const nsueldo = parseInt(sueldo) +parseInt(sueldo*psueldo);
+alert("el nuevo sueldo con el aumento es: "+nsueldo); */
+
+/* ejercicio 3 - un vendedor que tiene un sueldp base, recibe una comision del 30% del total de las ventas del
+mes, el quiere saber cuanto ganara en un mes que tuvo tres ventas*/
+
+let sueldo1 = prompt("ingrese el valor de su primera venta");
+let psueldo= 30/100;
+const nsueldo = parseInt(sueldo1) +parseInt(sueldo*psueldo);
+
+
+
